d5532a1b8dbfad83811c8fb73b466e88040c9515
Ihor Didyk <ihordidyk95@gmail.com>
2019-09-28 11:49:31 +0300

    init empty project

---
+++ src/main/java/com/demo/app/DemoAppCliApplication.java
@@ -0,0 +1,14 @@
+package com.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppCliApplication.class, args);
+    }
+
+
+}
